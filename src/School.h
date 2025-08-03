8a5e907b02eec2733df3b17faa201b7c141198eb
goatmanjah <school4elijah@gmail.com>
2025-08-02 21:54:23 -0400

    edits to porject

--- src/School.h
+++ src/School.h
@@ -3,4 +3,28 @@
 #ifndef PROJECT_3_SUPER_SCHOOLERS_SCHOOL_H
 #define PROJECT_3_SUPER_SCHOOLERS_SCHOOL_H
 
+#ifndef SCHOOL_H
+#define SCHOOL_H
+#include <string>
+using namespace std;
+
+struct School {
+    string name;
+    string county;
+    string level;
+
+    float testScore;
+    float graduationRate;
+    float safetyScore;
+    float stabilityScore;
+
+
+    float computeScore(float wTest, float wGrad, float wSafe, float wStable) const {
+        return testScore * wTest + graduationRate * wGrad + safetyScore * wSafe + stabilityScore * wStable;
+    }
+};
+
+
+
+
 #endif //PROJECT_3_SUPER_SCHOOLERS_SCHOOL_H
--- src/SchoolSystem.h
+++ src/SchoolSystem.h
@@ -1,8 +1,44 @@
-//
-// Created by elijah sherman on 8/2/25.
-//
 
 #ifndef PROJECT_3_SUPER_SCHOOLERS_SCHOOLSYSTEM_H
 #define PROJECT_3_SUPER_SCHOOLERS_SCHOOLSYSTEM_H
+
+#ifndef SCHOOLSYSTEM_H
+#define SCHOOLSYSTEM_H
+
+#include "School.h"
+#include "BTree.h"
+#include "MaxHeap.h"
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class SchoolSystem {
+private:
+    BTree tree;
+    unordered_map<string, vector<School>> countyIndex;
+
+public:
+
+    void loadData();
+
+
+    vector<School> filterSchools(const string& county, const string& level);
+
+
+    vector<School> recommendTopSchools(const string& county, const string& level,
+                                       float wTest, float wGrad, float wSafe, float wStable, int topN);
+
+
+    void normalize(School& school);
+
+    // helpers
+    void parseTestScores(const string& filename);
+    void parseGraduationRates(const string& filename);
+    void parseSafetyReports(const string& filename);
+    void parseStabilityScores(const string& filename);
+    void parseSchoolGrades(const string& filename);
+};
+
 
 #endif //PROJECT_3_SUPER_SCHOOLERS_SCHOOLSYSTEM_H
